472a3e2a78db3935a20f4af6ee82e3979a6d6806
YannickNGuessan <yannick.nguessan@istic.univ-rennes1.fr>
2017-02-08 16:05:38 +0100

    Update
    Part 1

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,33 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller("rechercheCrtl",["$scope", "$log",function($scope){
+	$scope.pokemons = [
+		{
+			id: 1,
+			nom: "pickachu"
+		},
+		{
+			id: 2,
+			nom: "toto"
+		},
+		{
+			id: 3,
+			nom: "pickacho"
+		},
+		{
+			id: 4,
+			nom: "don quichot"
+		},
+		{
+			id: 6,
+			nom: "danichu"
+		},
+	];
+	
+	$scope.go = function(){
+		$log.info($scope.model);
+	}
+			
+}]);
